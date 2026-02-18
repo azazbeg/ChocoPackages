052d030cacf957dc496d2af45a7beec9588766d8
Azaz Beg <azaz.beg@live.com>
2026-02-18 23:44:11 +0530

    Add dotnet 8.0 aspnetruntime package

---
+++ dotnet-8.0-aspnetruntime.8.0.15/tools/data.ps1
@@ -0,0 +1,10 @@
+﻿@{
+    PackageName = 'dotnet-8.0-aspnetruntime'
+    Url = 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.15/aspnetcore-runtime-8.0.15-win-x86.exe'
+    Checksum = '6e10ae6deaddf36b08900cffb7d55d9f1a7a66cb9b96d4a45e75abe64e178f61adfe8c2ba1bfd5ddad69634355283cd4ae35416d2367b545dda660f52905d991'
+    ChecksumType = 'sha512'
+    Url64 = 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.15/aspnetcore-runtime-8.0.15-win-x64.exe'
+    Checksum64 = 'e17e74f598030bc3a3f5c3cf8b6bb8f7873fe19a79deb70822dcc3aa7d5fff8dc0a77cc5a1833df9168ec634dacb7adb2837203bf75bc99d082b70bd678ef7ab'
+    ChecksumType64 = 'sha512'
+    AdditionalArgumentsToInstaller = $null
+}
