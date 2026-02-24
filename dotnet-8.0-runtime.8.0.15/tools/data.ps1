27f51ee1e2b830b19d7afe25cd83cfa7bd3b2f04
Azaz Beg <azaz.beg@live.com>
2026-02-24 13:09:02 +0530

    uploading of old versions dotnet

---
+++ dotnet-8.0-runtime.8.0.15/tools/data.ps1
@@ -0,0 +1,10 @@
+﻿@{
+    PackageName = 'dotnet-8.0-runtime'
+    Url = 'https://builds.dotnet.microsoft.com/dotnet/Runtime/8.0.15/dotnet-runtime-8.0.15-win-x86.exe'
+    Checksum = '2a2178fa7a4946c350c6c9ddc3fcdd9713a2130c2ba4f6d46d0350de8793f43fc64cbde4d1b40fca691591e68ffdccf0b915b48797315e5e3d794489e95e6c20'
+    ChecksumType = 'sha512'
+    Url64 = 'https://builds.dotnet.microsoft.com/dotnet/Runtime/8.0.15/dotnet-runtime-8.0.15-win-x64.exe'
+    Checksum64 = 'cc470cac15368ce0d4ea9f9cc7db8baf4290584a92f318f1f7df8397e440ee7e942678da8a56f44057f6fe0952bac30055d6d8b55d4f4f0c6559ad5dbf78783e'
+    ChecksumType64 = 'sha512'
+    AdditionalArgumentsToInstaller = $null
+}
---
+++ dotnet-8.0-sdk-4xx.8.0.409/tools/data.ps1
@@ -0,0 +1,10 @@
+﻿@{
+    PackageName = 'dotnet-8.0-sdk-4xx'
+    Url = 'https://builds.dotnet.microsoft.com/dotnet/Sdk/8.0.409/dotnet-sdk-8.0.409-win-x86.exe'
+    Checksum = '8cbfd5f0da10af335db89815e1465245e4fcc851da1e3c345b3774325db897a1ab0e7cbc9fe86807bf496281432e4bc06e2fd0bd50bf32fec1e4724d09e9b56c'
+    ChecksumType = 'sha512'
+    Url64 = 'https://builds.dotnet.microsoft.com/dotnet/Sdk/8.0.409/dotnet-sdk-8.0.409-win-x64.exe'
+    Checksum64 = '304012b1cb095a3c25bc65d9f3d07905a737fcdd60561b96f1c5a3440172b500fd5c17560634964672a5918bfaa713d789ea00ad8b7cae11bb23fe5a44218784'
+    ChecksumType64 = 'sha512'
+    AdditionalArgumentsToInstaller = $null
+}
